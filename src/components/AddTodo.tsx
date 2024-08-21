a210652507253f1f2f6d8b3588ef8fc02e2c407e
Örsan Günay <oersan.guenay@baywa.de>
2024-08-21 15:09:28 +0200

    Speichern in den LocalStorage

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,8 +1,27 @@
 import { InputButton } from "./InputButton";
+import {
+  setLocalStorageTodos,
+  getLocalStorageTodos,
+} from "../utils/localStorageHandler";
+import { ITodo } from "./Todo";
 
 const AddTodo = () => {
   const inputChangeHandler = (inputValue: string) => {
-    console.log(inputValue);
+    // 1. Ein neues Todo erstellen mit name = inputValue
+    const myNewTodo: ITodo = {
+      name: inputValue,
+      completed: false,
+      id: window.crypto.randomUUID(),
+    };
+
+    // 1.5 Alten Todos "zwischenspeichern"
+    const currTodos = getLocalStorageTodos();
+
+    // 2. Erstelle ein Array mit neuem Todo + alten Todos
+    const todoArr: ITodo[] = [...currTodos, myNewTodo];
+
+    // 3. Speichere Todos in LocalStorage
+    setLocalStorageTodos(todoArr);
   };
 
   return (
